35f0f8624e850ee4732ace90e83afc0832c578a7
Andrey Storchous <AndreyStorchous@coherentsolutions.com>
2016-12-16 19:10:13 +0300

    next video 100

---
+++ src/components/common/TextInput.js
@@ -0,0 +1,32 @@
+import React, {PropTypes} from 'react';
+
+const TextInput = (props) => {
+    const {name, label, onChange, placeholder, value, error} = props;
+  let wrapperClass = 'form-group';
+  if(error && error.length > 0) {
+    wrapperClass += " has-error";
+  }
+
+    return (
+        <div className={wrapperClass}>
+          <label htmlFor={name}>{label}</label>
+          <div classNmae="field">
+            <input
+              type="text"
+              name={name}
+              className="form-control"
+              placeholder={placeholder}
+              value={value}
+              onChange={onChange}
+            />
+            {error && <div className="alert alert-danger">{error}</div>}
+          </div>
+        </div>
+    );
+};
+
+TextInput.propTypes = {
+
+};
+
+export default TextInput;
